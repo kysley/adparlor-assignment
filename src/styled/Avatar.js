9c9101d9017d691f94ee3b836518cc66b3101083
Evan Kysley <evan@kysley.com>
2019-01-25 18:05:32 -0500

    Feat: Avatar primitive

---
+++ src/styled/Avatar.js
@@ -0,0 +1,25 @@
+import styled from '@emotion/styled'
+
+const AvatarBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: ${props => props.size + 'px'};
+  width: ${props => props.size + 'px'};
+  border-radius: 50px;
+  border: 2px solid #FFFFFF;
+  color: #FFF;
+  user-select: none;
+  display: ${props => props.inline ? 'inline' : null};
+  &:hover {
+    cursor: pointer;
+    background-color: rgb(129, 180, 196);
+  }
+`
+
+AvatarBox.defaultProps = {
+  size: 24,
+  inline: false,
+}
+
+export default AvatarBox

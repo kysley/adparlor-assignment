d3cf4ea52c4c71fe3f5a98bf7ebe8699bb06d66d
Evan Kysley <evan@kysley.com>
2019-01-25 18:08:28 -0500

    Feat: Add Header

--- src/routes.js
+++ src/routes.js
@@ -3,22 +3,24 @@ import { Switch, Redirect, Route } from 'react-router-dom'
 
 import { Container } from 'Styled/Grids'
 /* Import Components Start */
+import Header from 'Components/Header'
 /* Import Pages Start */
 import Library from 'Pages/Library'
 
 const Routes = () => (
-  // <Header />
-  <Container>
-    <Switch>
-      <Route
-        exact
-        path="/"
-        render={() => <Redirect to="/library" />} />
+  <>
+    <Header />
+    <Container>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={() => <Redirect to="/library" />} />
 
-      <Route exact path="/library" component={Library} />
-    </Switch>
-  </Container>
-  // </Wrapper>
+        <Route exact path="/library" component={Library} />
+      </Switch>
+    </Container>
+  </>
 )
 
 export default Routes

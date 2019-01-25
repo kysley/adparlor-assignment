b5d1aa4ef3e42241978315b289031d239a2924c2
Evan Kysley <evan@kysley.com>
2019-01-25 18:09:19 -0500

    Feat: initial replication of  header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,68 @@
 import React from 'react'
+import { withRouter } from 'react-router-dom'
 
-import HeaderGrid from 'Styled/Header'
+import HeaderGrid,
+{
+  SectionContainer,
+  Section,
+  ProfileContainer,
+  CompanyContainer,
+  CompanyName,
+  AddContainer,
+  TabContainer,
+  Tab,
+} from 'Styled/Header'
+import {
+  NotificationIcon,
+  CaretDownIcon,
+  AddUserIcon,
+  HomeIcon,
+  MediaIcon,
+  ProgressIcon,
+  CustomBPIcon,
+} from 'Styled/Icons'
+import AvatarBox from 'Styled/Avatar'
+import { OutlineButton } from 'Styled/Buttons'
 
 const Header = () => (
   <HeaderGrid>
-    hello
+    <SectionContainer>
+      <Section fakeActive to="/library">Blueprints</Section>
+      <Section to="/settings">Settings</Section>
+    </SectionContainer>
+    <ProfileContainer>
+      <NotificationIcon />
+      <AvatarBox>E</AvatarBox>
+    </ProfileContainer>
+    <CompanyContainer>
+      <CompanyName>Good Pet</CompanyName>
+      <CaretDownIcon />
+    </CompanyContainer>
+    <AddContainer>
+      <OutlineButton>
+        <AddUserIcon style={{ fill: '#FFFFFF' }} />
+        Add People
+      </OutlineButton>
+    </AddContainer>
+    <TabContainer>
+      <Tab fakeActive>
+        <HomeIcon style={{ fill: '#FFFFFF' }} />
+        Library
+      </Tab>
+      <Tab>
+        <ProgressIcon />
+        Progress
+      </Tab>
+      <Tab>
+        <MediaIcon />
+        Media
+      </Tab>
+      <Tab>
+        <CustomBPIcon />
+        Custom Blueprint
+      </Tab>
+    </TabContainer>
   </HeaderGrid>
 )
 
-export default Header
+export default withRouter(Header)
--- src/styled/Header.js
+++ src/styled/Header.js
@@ -1,5 +1,6 @@
 import styled from '@emotion/styled'
 import { css } from '@emotion/core'
+import { Link } from 'react-router-dom'
 
 /**
  * This is something I would consider putting into styled/Grids,
@@ -7,9 +8,118 @@ import { css } from '@emotion/core'
  * else so fine to sit in here (imo)
  */
 const headerGridStyles = css`
-
+  width: 100%;
+  display: grid;
+  grid-template-areas: 'section profile'
+                       'company add'
+                       'bottom bottom';
+  grid-template-rows: 2em 1fr 3em;
+  grid-template-columns: 1fr 200px;
+  background-color: rgb(13, 107, 138);
+  padding: 1em 10em 0 10em;
 `
 
-const HeaderGrid = styled.div`
+const HeaderGrid = styled.nav`
   ${headerGridStyles};
 `
+
+const SectionContainer = styled.div`
+  grid-area: section;
+`
+
+/**
+ * The active prop on RR@3.x.x NavLink seems to be broken in almost all situations..
+ * fakeActive is a quick (static) hack just for the sake of the assignment!
+ * this is also the source of the error in console.
+ */
+const Section = styled(Link)`
+  font-family: Theinhardt-Bold;
+  font-size: 1rem;
+  text-transform: uppercase;
+  grid-area: section;
+  text-decoration: none;
+  margin-right: 2em;
+
+  ${props => props.fakeActive ? 'color: #FFFFFF' : 'color: rgb(129, 180, 196)'};
+
+  :hover {
+    color: #FFFFFF;
+  }
+`
+
+const ProfileContainer = styled.div`
+  grid-area: profile;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+
+  >svg {
+    margin-right: 1em;
+  }
+`
+
+const CompanyContainer = styled.div`
+  grid-area: company;
+  display: flex;
+  align-items: baseline;
+`
+
+const CompanyName = styled.h2`
+  font-family: Theinhardt-Bold;
+  font-size: 2.25rem;
+  margin: .35em .25em .2em 0;
+  color: #FFFFFF;
+`
+
+const AddContainer = styled.div`
+  grid-area: add;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+`
+
+const TabContainer = styled.div`
+  grid-area: bottom;
+  background-color: rgb(11, 97, 128);
+  margin: 0 -10em;
+  padding: 0 10em;
+  display: flex;
+  flex-direction: row;
+`
+
+/**
+ * The active prop on RR@3.x.x NavLink seems to be broken in almost all situations..
+ * fakeActive is a quick (static) hack just for the sake of the assignment!
+ * this is also the source of the error in console.
+ */
+const Tab = styled.div`
+  display: flex;
+  align-items: center;
+  text-transform: uppercase;
+  font-family: Theinhardt-Bold;
+  cursor: pointer;
+
+  ${props => props.fakeActive ? 'color: #FFFFFF' : 'color: rgb(129, 180, 196)'};
+
+  :hover {
+    color: #FFFFFF;
+  }
+  margin-right: 2em;
+
+  >svg {
+    margin-right: .2em;
+  }
+`
+
+export default HeaderGrid
+
+export {
+  SectionContainer,
+  Section,
+  ProfileContainer,
+  CompanyContainer,
+  CompanyName,
+  AddContainer,
+  TabContainer,
+  Tab,
+}

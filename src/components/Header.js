6ef0b25c599978e628876f41757a91692ecab1c8
Evan Kysley <evan@kysley.com>
2019-01-24 20:16:19 -0500

    Feat: Init Header component

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import HeaderGrid from 'Styled/Header'
+
+const Header = () => (
+  <HeaderGrid>
+    hello
+  </HeaderGrid>
+)
+
+export default Header
---
+++ src/styled/Header.js
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled'
+import { css } from '@emotion/core'
+
+/**
+ * This is something I would consider putting into styled/Grids,
+ * however it's specific enough that we won't be using it anywhere
+ * else so fine to sit in here (imo)
+ */
+const headerGridStyles = css`
+
+`
+
+const HeaderGrid = styled.div`
+  ${headerGridStyles};
+`
